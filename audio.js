6b13e117b39759907d43806a2433b52b92f4487e
eehamidov <khmdve@gmail.com>
2016-11-13 02:24:53 +0300

    Add files via upload

--- audio.js
+++ audio.js
@@ -1,6 +1,6 @@
 function audioPlay() {
     var audio = new Audio();
-    audio.src = 'https://goo.gl/d6cWN1';
+    audio.src = 'https://goo.gl/Y8Y2J6';
     audio.autoplay = true;
-    audio.volume = 0.2;
+    audio.volume = 0.5;
 }
